da67d0e500e60fd95dbe352dd7ef29e21f205ff1
SanvicenteCesar <cesarsvp17@gmail.com>
2019-04-08 14:39:40 -0400

    SHA1 agregado

--- lib/pages/Inicio/RegisterPage.dart
+++ lib/pages/Inicio/RegisterPage.dart
@@ -12,94 +12,18 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPage extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: SecondaryColor,
-      body:  ListView.builder(
+    return new ListView.builder(
         itemCount: 1,
         itemBuilder: (context, index) {
           return new Container(
-            child: LoginPage(),
+            child: Text('HOLAAAAA'),
             margin: EdgeInsets.all(23),
             color: Colors.white,
           );
 
 
         },
-      ),
-    );
+      );
+
   }
 }
-
-class LoginPage extends StatefulWidget{
-
-  final String title;
-
-  LoginPage({Key key, this.title}) : super(key : key);
-
-  _LoginPageState createState() => _LoginPageState();
-
-  }
-
-class _LoginPageState extends State<LoginPage> {
-
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Registrarse'),
-
-        ),
-      body: Container(
-        padding: EdgeInsets.all(15),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                icon: Icon(Icons.email),
-
-              ),
-            ),
-            SizedBox(
-              height: 15.0,
-            ),
-
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                icon: Icon(Icons.vpn_key),
-
-              ),
-            ),
-            SizedBox(
-              height: 15.0,
-            ),
-            FlatButton(
-              child: Text('Login'),
-              color: PrimaryColor,
-              textColor: Colors.white,
-              onPressed: (){
-                FirebaseAuth.instance.signInWithEmailAndPassword(
-                    email: _emailController.text,
-                    password: _passwordController.text,).then((FirebaseUser user){
-                      Navigator.of(context).pushReplacement(CupertinoPageRoute(
-                        builder: (context) => Profile(),
-                      ));
-                });
-
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
--- lib/pages/Inicio/Tabs/profile.dart
+++ lib/pages/Inicio/Tabs/profile.dart
@@ -1,6 +1,14 @@
 import 'package:capturefacturas/pages/Inicio/RegisterPage.dart';
+import 'package:capturefacturas/pages/Inicio/Tabs/Home.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:capturefacturas/main.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn =GoogleSignIn();
 
 class Profile extends StatefulWidget {
   @override
@@ -8,6 +16,8 @@ class Profile extends StatefulWidget {
 }
 
 class _Profile extends State<Profile> {
+  bool _success;
+  String _userID;
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -15,10 +25,32 @@ class _Profile extends State<Profile> {
         body:  ListView.builder(
           itemCount: 1,
           itemBuilder: (context, index) {
-            return new Container(
-                child: RegisterPage(),
-              margin: EdgeInsets.all(23),
-              color: Colors.white,
+            return new Column(
+             children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 200),
+            child:  Column(
+              children: <Widget>[
+                Text('Iniciar Sesion',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+
+                ),
+                GoogleSignInButton(onPressed: (){
+                  _signInWithGoogle();
+
+                },darkMode: true,)
+
+              ],
+            ),
+          ),
+
+             ],
+
+
+
             );
 
 
@@ -26,5 +58,28 @@ class _Profile extends State<Profile> {
           },
         ),
     );
+  }
+  void _signInWithGoogle() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =  await googleUser.authentication;
+    final AuthCredential credential =GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    final FirebaseUser user = await _auth.signInWithCredential(credential);
+    assert(user.displayName != null);
+    assert(await user.getIdToken() != null);
+
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+    setState(() {
+      if (user != null) {
+        _success = true;
+        _userID = user.uid;
+        Navigator.of(context).pushReplacement(CupertinoPageRoute(
+          builder:(context) => Home(),
+        ));
+      }
+    });
   }
 }
