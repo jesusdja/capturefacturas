8d482af4f3b02a48329ede498380aadabd7bf7d6
pc <jesus.cortez.1991@gmail.com>
2019-04-08 09:40:30 -0400

    agregar libreria firebase

--- lib/pages/Inicio/Prinicipal.dart
+++ lib/pages/Inicio/Prinicipal.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'dart:async';
-import 'package:capturefacturas/pages/Model/modelodb.dart';
 
 class PrincipalPage extends StatefulWidget {
 
   _PrincipalState createState() => _PrincipalState();
 }
-
-//https://www.youtube.com/watch?v=uzkpDEG_4R4&t=19s
-
-final imagenesReferencia = FirebaseDatabase.instance.reference().child('imagenes');
 
 class _PrincipalState extends State<PrincipalPage> {
 
@@ -18,27 +13,6 @@ class _PrincipalState extends State<PrincipalPage> {
   @override
   Widget build(BuildContext context) {
 
-    List<modelodb> items;
-    //min 18:55
-
-    StreamSubscription<Event> _onSubcriptionAdd;
-    StreamSubscription<Event> _onSubcriptionEdit;
-
-    @override
-    initState(){
-      super.initState();
-      items = new List();
-
-      _onSubcriptionAdd = imagenesReferencia.onChildAdded.listen(_onAdd);
-      _onSubcriptionEdit = imagenesReferencia.onChildAdded.listen(_onSEdit);
-    }
-
-    @override
-    dispose(){
-      super.dispose();
-      _onSubcriptionAdd.cancel();
-      _onSubcriptionEdit.cancel();
-    }
 
     return Scaffold(
     );
